80ef42f982f46a74cf649adb07d274a798a094a8
Danny Shaw <code@dannyshaw.io>
2018-03-07 19:14:12 +1100

    supporter individual updates

--- models/SupporterIndividual.js
+++ models/SupporterIndividual.js
@@ -8,29 +8,13 @@ var Types = keystone.Field.Types;
 
 var SupporterIndividual = new keystone.List('SupporterIndividual', {
   map: { name: 'name' },
-  autokey: { from: 'name', path: 'slug', unique: true },
   sortable: true,
 });
 
-var dateParseFormat = 'DD/MM/YYYY';
 
 SupporterIndividual.add({
   name: { type: Types.Name, required: true },
 });
-// })
-// SupporterIndividual.defaultColumns = 'name';
 
-// Automatically set the date of donation when payment has been processsed
-SupporterIndividual.schema.methods.isPaymentCompleted = function() {
-  return this.state == true;
-};
-
-SupporterIndividual.schema.pre('save', function(next) {
-  if (this.isModified('paymentCompleted') && this.isPaymentCompleted() && !this.donatedOn) {
-    this.donatedOn = new Date();
-  }
-  next();
-});
-
-SupporterIndividual.defaultColumns = 'name, email, donationAmount, paymentCompleted, published';
+SupporterIndividual.defaultColumns = 'name';
 SupporterIndividual.register();
